accae4eb007fcca3190a3e14a94d0c785009d785
krikaz <pascal.bou@protonmail.com>
2019-05-08 14:13:06 +0200

    broken code, working on constructors

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,13 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. Window binding. If no object is binding, this bind to the default window object.
-* 2. Implicit Binding. This binds to the left object.
-* 3. New Binding. This binds to the new object created with an object constructor.
-* 4. Explicit Binding. This binds to the object passed as an argument with .call .apply or .bind.
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Window binding. If no object is binding, this bind to the default window object.
+ * 2. Implicit Binding. This binds to the left object.
+ * 3. New Binding. This binds to the new object created with an object constructor.
+ * 4. Explicit Binding. This binds to the object passed as an argument with .call .apply or .bind.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 // code example for Window Binding
@@ -15,21 +15,21 @@ console.log(this);
 
 // Principle 2
 // code example for Implicit Binding
-function Person (name) {
+function Person(name) {
   this.name = name;
 }
 
-Person.prototype.greet = function () {
+Person.prototype.greet = function() {
   console.log(`I am ${this.name}`);
-}
+};
 
 // Principle 3
 // code example for New Binding
-const jack = new Person('Jack');
-jack.greet()
+const jack = new Person("Jack");
+jack.greet();
 
 // Principle 4
 // code example for Explicit Binding
 const greeter = Person.prototype.greet;
-const jeff = new Person('Jeff');
+const jeff = new Person("Jeff");
 greeter.call(jeff);
